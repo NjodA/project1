15a20e713ecce017142a13fec21419598fa6bde4
NjodA <njod@njods-MacBook-Air.local>
2018-11-08 18:36:01 +0300

    food and weight controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,17 @@
 class FoodsController < ApplicationController
   def index
+    @foods = Food.all
   end
 
   def show
+    @food = Food.find_by(id: params[:id])
   end
 
   def edit
+    @food = Food.find_by(id: params[:id])
   end
 
   def new
+    @food = Food.new
   end
 end
--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,11 +1,17 @@
 class WeightsController < ApplicationController
   def index
-    
+    @weights = Weight.all
   end
 
   def show
+    @weight = Weight.find_by(id: params[:id])
   end
 
   def edit
+    @weight = Weight.find_by(id: params[:id])
+  end
+
+  def new
+    @weight = Weight.new
   end
 end
