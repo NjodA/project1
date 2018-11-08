37ebfb93fd1beeef2da49417b1a6df9dd8b29ea1
NjodA <njod@njods-MacBook-Air.local>
2018-11-08 18:42:55 +0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Weight.destroy_all
+Food.destroy_all
+
+Weight.create(amount: 50, user_id: 1, date: Time.now, comment: "first comment")
+Weight.create(amount: 60, user_id: 1, date: Time.now, comment: "second comment")
+Weight.create(amount: 70, user_id: 1, date: Time.now, comment: "third comment")
+
+Food.create(name: "first food", image: "", calories: 150)
+Food.create(name: "second food", image: "", calories: 250)
+Food.create(name: "third food", image: "", calories: 350)
